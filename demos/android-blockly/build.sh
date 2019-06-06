dc323b23953a71aa3cb3e32c63444c88c8f33903
Markus Herpich <markus@endocode.com>
2019-06-06 22:22:15 +0200

    [android-blockly] add android blockly demo

---
+++ demos/android-blockly/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+echo "########################"
+echo "# Running blockly demo #"
+echo "########################"
+
+if [ "$(uname -s)" = 'Linux' ]; then
+DEMOWD="$(dirname "$(readlink -f "$0")")"
+else
+DEMOWD="$(dirname "$(greadlink -f "$0")")"
+fi
+
+source ${DEMOWD}/../../build.inc
+pushd ${DEMOWD}
+setup_git_src https://github.com/google/blockly-android.git master blockly-android
+
+pushd blockly-android
+echo "Applying qmstr plugin to gradle build configuration"
+git reset --hard origin/master
+git am < ${DEMOWD}/0001-Apply-qmstr.patch
+#git am < ${DEMOWD}/0001-Update-gradle-version.patch
+popd
+
+echo "Waiting for qmstr-master server"
+eval $(qmstrctl start --wait --verbose)
+
+echo "[INFO] Start gradle build"
+#qmstrctl spawn qmstr/android-blocklydemo ./gradlew tasks
+qmstrctl spawn qmstr/android-blocklydemo ./gradlew assemble --stacktrace
+exit
+
+
+echo "[INFO] Build finished. Creating snapshot and triggering analysis."
+qmstrctl snapshot -O postbuild-snapshot.tar -f
+qmstrctl analyze --verbose
+
+echo "[INFO] Analysis finished. Creating snapshot and triggering reporting."
+qmstrctl snapshot -O postanalysis-snapshot.tar -f
+qmstrctl report --verbose
+
+qmstrctl quit
+
+echo "[INFO] Build finished."

b7be019d0125b6d53aaa495bc41d7fe496442d71
Markus Herpich <markus@endocode.com>
2019-06-06 22:21:19 +0200

    More java fixes

--- demos/java-jabref/build.sh
+++ demos/java-jabref/build.sh
@@ -16,7 +16,6 @@ pushd ${DEMOWD}
 setup_git_src https://github.com/JabRef/jabref.git v4.3 jabref
 
 pushd jabref
-git clean -fxd
 echo "Applying qmstr plugin to gradle build configuration"
 git am < ${DEMOWD}/add-qmstr.patch
 popd
